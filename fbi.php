f8da1869470649f04e17ae9eee8138e3ee5729e3
ozzy <65918625+ozzyisgreat@users.noreply.github.com>
2022-07-10 20:08:55 -0400

    Delete fbi.php

--- fbi.php
+++
@@ -1,4 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-</head><body><center><iframe src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&enablejsapi=1&autoplay=1&mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center></body></html>
