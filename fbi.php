5f1e528a710105d1f62c2c4653dc3747ac380c40
ozzy <65918625+ozzyisgreat@users.noreply.github.com>
2022-07-10 20:08:24 -0400

    Add files via upload

---
+++ fbi.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html>
+  <head>
+</head><body><center><iframe src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&enablejsapi=1&autoplay=1&mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center></body></html>
